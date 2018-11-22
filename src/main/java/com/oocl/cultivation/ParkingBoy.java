9cd3fbc6a030683a4f39752c248158adb829d64a
Liu Xia <lxconan@gmail.com>
2018-11-22 14:21:24 +0800

    [liuxia] feat: should not park a car if parking lot is full.

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -5,20 +5,28 @@ public class ParkingBoy {
     private final ParkingLot parkingLot;
     private String lastErrorMessage;
 
+    private void setLastError(Throwable error) {
+        lastErrorMessage = error.getMessage();
+    }
+
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
 
     public ParkingTicket park(Car car) {
-        ParkingTicket ticket = parkingLot.park(car);
-        return ticket;
+        try {
+            return parkingLot.park(car);
+        } catch (ParkingException error) {
+            setLastError(error);
+            return null;
+        }
     }
 
     public Car fetch(ParkingTicket ticket) {
         try {
             return parkingLot.fetch(ticket);
         } catch (ParkingException error) {
-            lastErrorMessage = error.getMessage();
+            setLastError(error);
             return null;
         }
     }
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,9 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
+    private final int capacity;
     private Map<ParkingTicket, Car> cars = new HashMap<>();
 
+    public ParkingLot() {
+        this(10);
+    }
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
     public ParkingTicket park(Car car) {
+        if (cars.size() == capacity) {
+            throw new ParkingException("The parking lot is full.");
+        }
+
         ParkingTicket ticket = new ParkingTicket();
         cars.put(ticket, car);
         return ticket;
